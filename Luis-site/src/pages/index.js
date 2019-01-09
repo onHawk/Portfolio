ddd8ba76d12831530d4596fa2b9e251762bac90f
onHawk <luisd824@gmail.com>
2019-01-09 18:43:03 -0500

    adjust reposnsiveness of about section

--- Luis-site/src/pages/index.js
+++ Luis-site/src/pages/index.js
@@ -5,18 +5,18 @@ import Layout from '../components/layout'
 // import Image from '../components/image'
 // import SEO from '../components/seo'
 import About from '../components/about'
+import Projects from '../components/projects'
 
 const IndexPage = () => (
-  <>
-    <Layout />
-    <div className="container">
-      <About />
-    </div>
+  <Layout>
+    <About />
+    <Projects />
+
     <footer>
       © {new Date().getFullYear()}
       {` `}
     </footer>
-  </>
+  </Layout>
 )
 
 export default IndexPage
