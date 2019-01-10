3255f09dd2a5513ad73f14f71bcb91db52608e41
onHawk <luisd824@gmail.com>
2019-01-10 17:43:29 -0500

    style project section with description

--- Luis-site/src/components/projects.js
+++ Luis-site/src/components/projects.js
@@ -1,40 +1,63 @@
 import React from 'react'
 
-import { Row, Col } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 
 const Projects = () => {
   return (
-    <div style={{ minWidth: '100%' }} id="projects">
+    <div id="projects">
       <div className="titles">
         <h1>Projects</h1>
       </div>
 
-      <div className="project_grid">
-        <Row>
-          <Col xl="4" l="4" md="5" s="12" xs="12" className="p" id="gmmm">
-            <a href="www.givememymoney.app" target="_blank">
+      <Container style={{ padding: '0' }}>
+        <Row style={{ margin: '.5rem 0' }}>
+          <Col md="6" s="12" className="p" id="gmmm" />
+
+          <Col md="6" s="12" className="details">
+            <a
+              href="www.givememymoney.app"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: '#e86c62' }}
+            >
               GiveMeMyMoney
             </a>
           </Col>
-          <Col
-            xl="4"
-            l="4"
-            md="5"
-            s="12"
-            xs="12"
-            className="p"
-            id="lambdanotes"
-          >
-            <a href="l-notes.netlify.com" target="_blank">
+        </Row>
+
+        <Row style={{ margin: '.5rem 0' }}>
+          <Col md="6" s="12" className="p" id="lambdanotes" />
+
+          <Col md="6" s="12" className="details">
+            <a
+              href="l-notes.netlify.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: 'crimson' }}
+            >
               {' '}
               Lambda Notes
             </a>
           </Col>
-          <Col xl="4" l="4" md="5" s="12" xs="12" className="p" id="chronicle">
-            <a>Chronicle</a>
+        </Row>
+
+        <Row style={{ margin: '.5rem 0' }}>
+          <Col md="6" s="12" className="p" id="chronicle" />
+
+          <Col md="6" s="12" className="details">
+            <a
+              href="http://chronicle-memory.herokuapp.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h1 style={{ color: '#a2daff' }}>Chronicle</h1>
+            </a>
+            <a href="https://github.com/onHawk/Memory">
+              <p>github</p>
+            </a>
           </Col>
         </Row>
-      </div>
+      </Container>
     </div>
   )
 }
