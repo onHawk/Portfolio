15bbcbc1555c1a4bcec571c777ba9cb8b728a6cb
onHawk <luisd824@gmail.com>
2019-01-11 15:44:09 -0500

    fix project links to direct correctly and style change

--- Luis-site/src/components/projects.js
+++ Luis-site/src/components/projects.js
@@ -11,23 +11,31 @@ const Projects = () => {
         <h1>Projects</h1>
       </div>
 
-      <Container style={{ padding: '0' }}>
-        <Row style={{ margin: '.5rem 0' }}>
-          <Col md="6" s="12" className="p" id="gmmm" />
+      <Container>
+        <Row>
+          <Col xl="4" md="5" s="12" className="p" id="gmmm" />
 
-          <Col md="6" s="12" className="details">
-            <a
+          <Col xl="5" md="6" s="12" className="details">
+            {/* <a
               href="www.givememymoney.app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h1 style={{ color: '#e86c62' }}>GiveMeMyMoney</h1>
-            </a>
+            </a> */}
+            <h1 style={{ color: '#e86c62' }}>GiveMeMyMoney</h1>
             <div className="details-links">
-              <a href="www.givememymoney.app">
+              <a
+                href="https://github.com/Lambda-School-Labs/CS8-payme"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <GoCode />
               </a>
-              <a href="https://github.com/Lambda-School-Labs/CS8-payme">
+              <a
+                href="https://www.givememymoney.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <GoDeviceDesktop />
               </a>
             </div>
@@ -35,46 +43,62 @@ const Projects = () => {
           </Col>
         </Row>
 
-        <Row style={{ margin: '.5rem 0' }}>
-          <Col md="6" s="12" className="p" id="lambdanotes" />
+        <Row>
+          <Col xl="4" md="5" s="12" className="p" id="lambdanotes" />
 
-          <Col md="6" s="12" className="details">
-            <a
+          <Col xl="5" md="6" s="12" className="details">
+            {/* <a
               href="l-notes.netlify.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h1 style={{ color: 'crimson' }}>Lambda Notes</h1>
-            </a>
+            </a> */}
+            <h1 style={{ color: 'crimson' }}>Lambda Notes</h1>
 
             <div className="details-links">
-              <a href="l-notes.netlify.com">
+              <a
+                href="https://github.com/onHawk/front-end-project-week"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <GoCode />
               </a>
-              <a href="https://github.com/onHawk/front-end-project-week">
+              <a
+                href="l-notes.netlify.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <GoDeviceDesktop />
               </a>
             </div>
           </Col>
         </Row>
 
-        <Row style={{ margin: '.5rem 0' }}>
-          <Col md="6" s="12" className="p" id="chronicle" />
+        <Row>
+          <Col xl="4" md="5" s="12" className="p" id="chronicle" />
 
-          <Col md="6" s="12" className="details">
-            <a
+          <Col xl="5" md="6" s="12" className="details">
+            {/* <a
               href="http://chronicle-memory.herokuapp.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h1 style={{ color: '#a2daff' }}>Chronicle</h1>
-            </a>
+            </a> */}
+            <h1 style={{ color: '#a2daff' }}>Chronicle</h1>
 
             <div className="details-links">
-              <a href="https://github.com/onHawk/Memory">
+              <a
+                href="https://github.com/onHawk/Memory"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <GoCode />
               </a>
-              <a href="http://chronicle-memory.herokuapp.com">
+              <a
+                href="https://chronicle-memory.herokuapp.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <GoDeviceDesktop />
               </a>
             </div>
