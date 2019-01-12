da986f45bfec8c9e812b8d50b99fdf9057e47c42
onHawk <luisd824@gmail.com>
2019-01-12 18:55:58 -0500

    contact section styles

--- Luis-site/src/components/contact.js
+++ Luis-site/src/components/contact.js
@@ -2,36 +2,59 @@ import React from 'react'
 
 import { Container, Row, Col } from 'reactstrap'
 
+import {
+  FaGithub,
+  FaLinkedin,
+  FaMobileAlt,
+  FaAddressCard,
+  FaEnvelope,
+  FaGlobe,
+} from 'react-icons/fa'
+
 const Contact = () => {
   return (
     <Container id="contact">
-      <form>
+      <form className="form">
         <Row>
-          <Col className="form-groups">
+          <Col xl="6" md="6" s="12" className="form-groups">
             <label>Name</label>
-            <input name="name" placeholder="Name..." type="text" />
+            <input name="name" type="text" />
           </Col>
 
-          <Col className="form-groups">
+          <Col xl="6" md="6" s="12" className="form-groups">
             <label>Email</label>
-            <input name="email" placeholder="email..." type="email" />
+            <input name="email" type="email" />
           </Col>
         </Row>
 
         <Row>
           <Col id="messagefield">
             <label>Message</label>
-            <textarea
-              name="message"
-              placeholder="Can't forget what to say..."
-            />
+            <textarea name="message" />
           </Col>
         </Row>
-        <Row style={{ margin: '20px' }}>
-          <Col>
-            <button className="form-button">Send</button>
+
+        <Row>
+          <Col className="myinfo">
+            <FaAddressCard size="34px" />
+            <p> 770.778.9995 </p>
+          </Col>
+
+          <Col className="myinfo">
+            <FaEnvelope size="34px" />
+            <a href="mailto:luisd824@gmail.com">luisd824@gmail.com</a>
+          </Col>
+
+          <Col className="myinfo">
+            <FaGlobe size="34" />
+            <p style={{ fonztSize: '18px', margin: '0' }}>
+              7472 Majestic Lane <br />
+              Flowery Branch, Ga 30542
+            </p>
           </Col>
         </Row>
+
+        <button className="form-button">Send</button>
       </form>
     </Container>
   )
