bc3c61877b04b4418e50a6e0f44d1a59decab692
onHawk <luisd824@gmail.com>
2019-01-11 19:26:17 -0500

    restyle project section

--- Luis-site/src/components/projects.js
+++ Luis-site/src/components/projects.js
@@ -6,106 +6,95 @@ import { GoCode, GoDeviceDesktop } from 'react-icons/go'
 
 const Projects = () => {
   return (
-    <div id="projects">
-      <div className="titles">
-        <h1>Projects</h1>
-      </div>
+    <Container id="projects">
+      <Row className="grid">
+        <Col xl="4" md="6" className="p" id="gmmm">
+          <div className="p-name">
+            <h1 style={{ backgroundColor: '#e86c62' }}>
+              GiveMe
+              <br />
+              MyMoney
+            </h1>
+          </div>
+        </Col>
 
-      <Container>
-        <Row>
-          <Col xl="4" md="5" s="12" className="p" id="gmmm" />
-
-          <Col xl="5" md="6" s="12" className="details">
-            {/* <a
-              href="www.givememymoney.app"
+        <Col xl="1" md="2" className="details">
+          <div className="details-links">
+            <a
+              href="https://github.com/Lambda-School-Labs/CS8-payme"
               target="_blank"
               rel="noopener noreferrer"
             >
-            </a> */}
-            <h1 style={{ color: '#e86c62' }}>GiveMeMyMoney</h1>
-            <div className="details-links">
-              <a
-                href="https://github.com/Lambda-School-Labs/CS8-payme"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GoCode />
-              </a>
-              <a
-                href="https://www.givememymoney.app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GoDeviceDesktop />
-              </a>
-            </div>
-            <p />
-          </Col>
-        </Row>
+              <GoCode />
+            </a>
+            <a
+              href="https://www.givememymoney.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GoDeviceDesktop />
+            </a>
+          </div>
+          <p />
+        </Col>
+      </Row>
 
-        <Row>
-          <Col xl="4" md="5" s="12" className="p" id="lambdanotes" />
+      <Row className="grid">
+        <Col xl="4" md="6" className="p" id="lambdanotes">
+          <div className="p-name">
+            <h1 style={{ backgroundColor: '#A0011E' }}>Lambda Notes</h1>
+          </div>
+        </Col>
 
-          <Col xl="5" md="6" s="12" className="details">
-            {/* <a
+        <Col xl="1" md="2" className="details">
+          <div className="details-links">
+            <a
+              href="https://github.com/onHawk/front-end-project-week"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GoCode />
+            </a>
+            <a
               href="l-notes.netlify.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-            </a> */}
-            <h1 style={{ color: 'crimson' }}>Lambda Notes</h1>
+              <GoDeviceDesktop />
+            </a>
+          </div>
+        </Col>
+      </Row>
 
-            <div className="details-links">
-              <a
-                href="https://github.com/onHawk/front-end-project-week"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GoCode />
-              </a>
-              <a
-                href="l-notes.netlify.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GoDeviceDesktop />
-              </a>
-            </div>
-          </Col>
-        </Row>
+      <Row className="grid">
+        <Col xl="4" md="6" className="p" id="chronicle">
+          <div className="p-name">
+            <h1 style={{ backgroundColor: '#a2daffbe' }}>
+              tbd Memory Chronicles
+            </h1>
+          </div>
+        </Col>
 
-        <Row>
-          <Col xl="4" md="5" s="12" className="p" id="chronicle" />
-
-          <Col xl="5" md="6" s="12" className="details">
-            {/* <a
-              href="http://chronicle-memory.herokuapp.com"
+        <Col xl="1" md="2" className="details">
+          <div className="details-links">
+            <a
+              href="https://github.com/onHawk/Memory"
               target="_blank"
               rel="noopener noreferrer"
             >
-            </a> */}
-            <h1 style={{ color: '#a2daff' }}>Chronicle</h1>
-
-            <div className="details-links">
-              <a
-                href="https://github.com/onHawk/Memory"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GoCode />
-              </a>
-              <a
-                href="https://chronicle-memory.herokuapp.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GoDeviceDesktop />
-              </a>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+              <GoCode />
+            </a>
+            <a
+              href="https://chronicle-memory.herokuapp.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GoDeviceDesktop />
+            </a>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
