4fd79749f4fdeb982ffca97b269d8c7d4df6006d
onHawk <luisd824@gmail.com>
2019-01-12 19:26:30 -0500

    moved my info to footer

--- Luis-site/src/components/contact.js
+++ Luis-site/src/components/contact.js
@@ -2,15 +2,6 @@ import React from 'react'
 
 import { Container, Row, Col } from 'reactstrap'
 
-import {
-  FaGithub,
-  FaLinkedin,
-  FaMobileAlt,
-  FaAddressCard,
-  FaEnvelope,
-  FaGlobe,
-} from 'react-icons/fa'
-
 const Contact = () => {
   return (
     <Container id="contact">
@@ -31,26 +22,6 @@ const Contact = () => {
           <Col id="messagefield">
             <label>Message</label>
             <textarea name="message" />
-          </Col>
-        </Row>
-
-        <Row>
-          <Col className="myinfo">
-            <FaAddressCard size="34px" />
-            <p> 770.778.9995 </p>
-          </Col>
-
-          <Col className="myinfo">
-            <FaEnvelope size="34px" />
-            <a href="mailto:luisd824@gmail.com">luisd824@gmail.com</a>
-          </Col>
-
-          <Col className="myinfo">
-            <FaGlobe size="34" />
-            <p style={{ fonztSize: '18px', margin: '0' }}>
-              7472 Majestic Lane <br />
-              Flowery Branch, Ga 30542
-            </p>
           </Col>
         </Row>
 
--- Luis-site/src/pages/index.js
+++ Luis-site/src/pages/index.js
@@ -8,6 +8,16 @@ import About from '../components/about'
 import Projects from '../components/projects'
 import Contact from '../components/contact'
 
+import { Container, Row, Col } from 'reactstrap'
+import {
+  FaGithub,
+  FaLinkedin,
+  FaMobileAlt,
+  FaAddressCard,
+  FaEnvelope,
+  FaGlobe,
+} from 'react-icons/fa'
+
 const IndexPage = () => (
   <Layout>
     <About />
@@ -15,10 +25,44 @@ const IndexPage = () => (
       <h2>My Projects</h2>
     </div>
     <Projects />
+    <div className="titles">
+      <h2>Contact me</h2>
+    </div>
     <Contact />
-    <footer>
-      © {new Date().getFullYear()}
-      {` `}
+    <footer className="foot">
+      <Row style={{ marginTop: '1rem' }}>
+        <Col className="myinfo">
+          <FaAddressCard size="34px" />
+          <p> 770.778.9995 </p>
+        </Col>
+
+        <Col className="myinfo">
+          <FaEnvelope size="34px" />
+          <a href="mailto:luisd824@gmail.com">luisd824@gmail.com</a>
+        </Col>
+
+        <Col className="myinfo" md="12">
+          <FaGlobe size="34" />
+          <p style={{ fonztSize: '18px', margin: '0' }}>
+            7472 Majestic Lane <br />
+            Flowery Branch, Ga 30542
+          </p>
+        </Col>
+      </Row>
+
+      <Row className="foot-links">
+        <Col>
+          <p style={{ margin: '9px' }}>© {new Date().getFullYear()}</p>
+        </Col>
+        <Col>
+          <FaGithub size="34" color="#333333" />
+        </Col>
+        <Col>
+          <a href="https://www.linkedin.com/in/luis-diaz-herrera-11a306168/">
+            <FaLinkedin size="34" color="#333333" />
+          </a>
+        </Col>
+      </Row>
     </footer>
   </Layout>
 )
