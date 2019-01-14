0dc6b6342fdd4bbc11529775c4b4745bcf3fa004
onHawk <luisd824@gmail.com>
2019-01-14 13:38:22 -0500

    fix footer height not aligning properly. new colors

--- Luis-site/src/pages/index.js
+++ Luis-site/src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => (
     </div>
     <Contact />
     <footer className="foot">
-      <Row style={{ marginTop: '1rem' }}>
+      <Row>
         <Col className="myinfo">
           <FaAddressCard size="34px" />
           <p> 770.778.9995 </p>
@@ -40,7 +40,9 @@ const IndexPage = () => (
           <FaEnvelope size="34px" />
           <a href="mailto:luisd824@gmail.com">luisd824@gmail.com</a>
         </Col>
+      </Row>
 
+      <Row>
         <Col className="myinfo" md="12">
           <FaGlobe size="34" />
           <p style={{ fonztSize: '18px', margin: '0' }}>
@@ -50,16 +52,22 @@ const IndexPage = () => (
         </Col>
       </Row>
 
-      <Row className="foot-links">
-        <Col>
-          <p style={{ margin: '9px' }}>© {new Date().getFullYear()}</p>
+      <Row style={{ height: '105px' }}>
+        <Col className="foot-links">
+          <p style={{ margin: '9px', color: '#5fa5ff73' }}>
+            © {new Date().getFullYear()}
+          </p>
         </Col>
-        <Col>
-          <FaGithub size="34" color="#333333" />
+
+        <Col className="foot-links">
+          <a href="www.github.com/onHawk">
+            <FaGithub size="34" />
+          </a>
         </Col>
-        <Col>
+
+        <Col className="foot-links">
           <a href="https://www.linkedin.com/in/luis-diaz-herrera-11a306168/">
-            <FaLinkedin size="34" color="#333333" />
+            <FaLinkedin size="34" />
           </a>
         </Col>
       </Row>
