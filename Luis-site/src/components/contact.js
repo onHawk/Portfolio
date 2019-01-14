ea202742ad3d25271881d65c962b627821db5c92
onHawk <luisd824@gmail.com>
2019-01-14 14:01:37 -0500

    fix styles causing overflow

--- Luis-site/src/components/contact.js
+++ Luis-site/src/components/contact.js
@@ -6,37 +6,19 @@ const Contact = () => {
   return (
     <Container id="contact">
       <form className="form">
-        <Row>
-          <Col
-            xl="6"
-            md="6"
-            s="12"
-            className="form-groups"
-            style={{
-              borderRight: '1px solid #5fa5ff73',
-              borderRadius: '3px 0 0 0 ',
-            }}
-          >
+        <Row style={{ margin: '0 auto' }}>
+          <Col xl="6" md="6" xs="12" className="form-groups">
             <label>Name</label>
             <input name="name" type="text" />
           </Col>
 
-          <Col
-            xl="6"
-            md="6"
-            s="12"
-            className="form-groups"
-            style={{
-              borderLeft: '1px solid #5fa5ff73',
-              borderRadius: '0 3px 0 0 ',
-            }}
-          >
+          <Col xl="6" md="6" s="12" className="form-groups">
             <label>Email</label>
             <input name="email" type="email" />
           </Col>
         </Row>
 
-        <Row>
+        <Row style={{ margin: '0 auto' }}>
           <Col id="messagefield">
             <label>Message</label>
             <textarea name="message" />
