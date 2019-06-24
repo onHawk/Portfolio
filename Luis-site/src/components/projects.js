8db62916698d2de5851d101ab9d1814016971b1a
Luisdh <luisd824@gmail.com>
2019-06-24 10:36:02 -0400

    reformat projects

--- Luis-site/src/components/projects.js
+++ Luis-site/src/components/projects.js
@@ -11,24 +11,17 @@ const Projects = () => {
         <h2>My Portfolio</h2>
       </div>
       <Row className="grid">
-        <Col xl="3" md="4" className="p" id="gmmm">
-          <div className="p-name">
-            <h1>
-              GiveMe
-              <br />
-              MyMoney
-            </h1>
-            <p>
-              Web app built in React frontend and Node and Mongodb backend.
-              Aimed to streamline invoice sending and organization. I built this
-              as part of a team where I handled the reminder feature to send
-              either a sms or email message to let a client know when an invoice
-              is due.
-            </p>
-          </div>
-        </Col>
+        <Col xl="3" l="5" md="5" className="p" id="gmmm" />
 
-        <Col md="1" className="details">
+        <Col md="4" className="details">
+          <h4 style={{ padding: '5px' }}>GiveMeMyMoney</h4>
+          <p>
+            Web app built in React frontend and Node and Mongodb backend. Aimed
+            to streamline invoice sending and organization. I built this as part
+            of a team where I handled the reminder feature to send either a sms
+            or email message to let a client know when an invoice is due.
+          </p>
+
           <div className="details-links">
             <a
               href="https://github.com/Lambda-School-Labs/CS8-payme"
@@ -37,6 +30,7 @@ const Projects = () => {
             >
               <GoCode />
             </a>
+
             <a
               href="https://www.givememymoney.app"
               target="_blank"
@@ -46,20 +40,16 @@ const Projects = () => {
             </a>
           </div>
         </Col>
-        {/* </Row> */}
+      </Row>
 
-        {/* <Row className="grid"> */}
-        <Col xl="3" md="4" className="p" id="lambdanotes">
-          <div className="p-name">
-            <h1>Lambda Notes</h1>
-            <p>
-              A note taking app built in React frontend, Node and MongoDB
-              backend.
-            </p>
-          </div>
-        </Col>
+      <Row className="grid">
+        <Col xl="3" l="5" md="5" className="p" id="lambdanotes" />
 
-        <Col xl="1" md="1" className="details">
+        <Col md="4" className="details">
+          <h4>Lambda Notes</h4>
+          <p>
+            A note taking app built in React frontend, Node and MongoDB backend.
+          </p>
           <div className="details-links">
             <a
               href="https://github.com/Luisdh/front-end-project-week"
@@ -80,18 +70,15 @@ const Projects = () => {
       </Row>
 
       <Row className="grid">
-        <Col xl="3" md="5" className="p" id="chronicle">
-          <div className="p-name">
-            <h1>Memory Chronicles</h1>
-            <p>
-              Journal app built in React frontend, Node and MongoDB backend.I
-              aimed to have it be as straight forward as possible without
-              convoluting the task of journaling.
-            </p>
-          </div>
-        </Col>
+        <Col xl="3" l="5" md="5" className="p" id="chronicle" />
 
-        <Col xl="1" md="1" className="details">
+        <Col md="4" className="details">
+          <h4>Memory Chronicles</h4>
+          <p>
+            Journal app built in React frontend, Node and MongoDB backend.I
+            aimed to have it be as straight forward as possible without
+            convoluting the task of journaling.
+          </p>
           <div className="details-links">
             <a
               href="https://github.com/Luisdh/Memory"
@@ -109,25 +96,22 @@ const Projects = () => {
             </a>
           </div>
         </Col>
+      </Row>
 
-        {/* </Row>
-      <Row className="grid"> */}
-        <Col xl="3" md="5" className="p" id="quicknews">
-          <div className="p-name">
-            <h1 style={{ color: '#333333' }}>Quicknews</h1>
-            <p>
-              News browsing mobile app I made in React-Native as a way to learn
-              more about it. I used the News api from{' '}
-              <span style={{ color: 'green', fontWeight: 'bold' }}>
-                newsapi.org
-              </span>
-              . It grabs top headlines given a news outlet or a topic category.
-              Apk is available.
-            </p>
-          </div>
-        </Col>
+      <Row className="grid">
+        <Col xl="3" md="5" className="p" id="quicknews" />
 
-        <Col xl="1" md="1" className="details">
+        <Col md="4" className="details">
+          <h4 style={{ color: '#333333' }}>Quicknews</h4>
+          <p>
+            News browsing mobile app I made in React-Native as a way to learn
+            more about it. I used the News api from{' '}
+            <span style={{ color: 'green', fontWeight: 'bold' }}>
+              newsapi.org
+            </span>
+            . It grabs top headlines given a news outlet or a topic category.
+            Apk is available.
+          </p>
           <div className="details-links">
             <a
               href="https://github.com/Luisdh/quicknews"
