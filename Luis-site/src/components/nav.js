49cbca92587bcf60f65fbf4acb43e9883e75adb2
Luisdh <luisd824@gmail.com>
2019-07-01 15:19:35 -0400

    update resume

--- Luis-site/src/components/nav.js
+++ Luis-site/src/components/nav.js
@@ -19,7 +19,7 @@ const Nav = () => {
         </a>
 
         <a
-          href="https://drive.google.com/file/d/14B9AQ7ZZBL4bnsifXvQD-oE4aRlJZ2qt/view?usp=sharing"
+          href="https://drive.google.com/file/d/16mi33wSsc5jXUacRGlzmY2P437cFwgKb/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           className="nav-i"
