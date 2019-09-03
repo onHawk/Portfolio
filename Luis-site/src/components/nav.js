5b87dce201ca36b0e5ba9672d690602694810389
Luisdh <luisd824@gmail.com>
2019-09-03 19:36:24 -0400

    add nav shadow animation on scroll

--- Luis-site/src/components/nav.js
+++ Luis-site/src/components/nav.js
@@ -1,42 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import '../sass/nav.scss'
 
-const Nav = () => {
-  return (
-    <div className="bar">
-      <ul className="nav">
-        <a className="nav-i" href="#about">
-          <li className="nav-anim" style={{ animationDelay: '0s' }}>
-            About
-          </li>
-        </a>
+class Nav extends Component {
+  state = {
+    offSet: 0,
+  }
 
-        <a className="nav-i" href="#projects">
-          <li className="nav-anim" style={{ animationDelay: '.1s' }}>
-            Portfolio
-          </li>
-        </a>
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
 
-        <a className="nav-i" href="#contact">
-          <li className="nav-anim" style={{ animationDelay: '.2s' }}>
-            Contact
-          </li>
-        </a>
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
 
-        <a
-          href="https://drive.google.com/file/d/18NctLXLu8ybIk-WlY1Dns_1uQGOzie7W/view?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="nav-i"
-        >
-          <li className="nav-anim" style={{ animationDelay: '.3s' }}>
-            Resume
-          </li>
-        </a>
-      </ul>
-    </div>
-  )
+  handleScroll = e => {
+    this.setState({ offSet: window.pageYOffset })
+  }
+
+  render() {
+    const { offSet } = this.state
+    console.log(this.state.offSet)
+
+    return (
+      <div
+        className="bar"
+        style={
+          offSet <= 0
+            ? null
+            : { boxShadow: 'rgb(24, 31, 46) 0px 10px 20px -3px' }
+        }
+      >
+        <ul className="nav">
+          <a className="nav-i" href="#about">
+            <li className="nav-anim" style={{ animationDelay: '0s' }}>
+              About
+            </li>
+          </a>
+
+          <a className="nav-i" href="#projects">
+            <li className="nav-anim" style={{ animationDelay: '.1s' }}>
+              Portfolio
+            </li>
+          </a>
+
+          <a className="nav-i" href="#contact">
+            <li className="nav-anim" style={{ animationDelay: '.2s' }}>
+              Contact
+            </li>
+          </a>
+
+          <a
+            href="https://drive.google.com/file/d/18NctLXLu8ybIk-WlY1Dns_1uQGOzie7W/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-i"
+          >
+            <li className="nav-anim" style={{ animationDelay: '.3s' }}>
+              Resume
+            </li>
+          </a>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Nav
