27f3c3fbadf4e07449f6a1396a2b7a95a699de4a
Luisdh <luisd824@gmail.com>
2019-09-09 15:59:27 -0400

    adjust about me section  responsivness

--- Luis-site/src/components/about.js
+++ Luis-site/src/components/about.js
@@ -10,23 +10,25 @@ const About = () => {
       <div className="titles">
         <h2>About me</h2>
       </div>
-      <div id="me" />
-      <div className="text_container">
-        <p>
-          I am a Fullstack Engineer passionate about all aspects of web and
-          mobile development. My core experience is in React, node.js and
-          MongoDB.
-          <br />
-          <br />
-          I also have experience working on both iOS and Android apps using
-          React Native. I am always striving to learn more about software
-          development best practices and apply them to building best-in-class
-          software.
-          <br />
-          <br />
-          When I'm not at my computer, I like to spend my time with my dog, take
-          hikes and relax with music to clear my mind.
-        </p>
+      <div className="about_container">
+        <div id="me" />
+        <div className="text_container">
+          <p>
+            I am a Fullstack Engineer passionate about all aspects of web and
+            mobile development. My core experience is in React, node.js and
+            MongoDB.
+            <br />
+            <br />
+            I also have experience working on both iOS and Android apps using
+            React Native. I am always striving to learn more about software
+            development best practices and apply them to building best-in-class
+            software.
+            <br />
+            <br />
+            When I'm not at my computer, I like to spend my time with my dog,
+            take hikes and relax with music to clear my mind.
+          </p>
+        </div>
       </div>
     </div>
   )
